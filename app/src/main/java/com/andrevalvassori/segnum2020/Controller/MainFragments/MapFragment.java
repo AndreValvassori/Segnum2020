c53cc9e3032be0ac168ef1caeb4be2bd0db8afd6
Andre Victor Stephanes Valvassori <avalvassori@brdom02.com>
2020-04-08 16:03:24 -0300

    heatmap + map fix

--- app/src/main/java/com/andrevalvassori/segnum2020/Controller/MainFragments/MapFragment.java
+++ app/src/main/java/com/andrevalvassori/segnum2020/Controller/MainFragments/MapFragment.java
@@ -2,13 +2,18 @@ package com.andrevalvassori.segnum2020.Controller.MainFragments;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.andrevalvassori.segnum2020.DTO.event.EventDTO;
 import com.andrevalvassori.segnum2020.R;
+import com.andrevalvassori.segnum2020.Singleton.DataStore;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -17,13 +22,22 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.*;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.maps.android.heatmaps.Gradient;
+import com.google.maps.android.heatmaps.HeatmapTileProvider;
+
+import java.util.ArrayList;
 
 public class MapFragment extends SupportMapFragment implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    MapView gMapView;
-    View v;
-/*
+    private ArrayList<LatLng> heatMapMarkers = new ArrayList<LatLng>();
+    private HeatmapTileProvider mProvider;
+    private TileOverlay mOverlay;
+
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        //setContentView(R.layout.fragment_map);
@@ -32,35 +46,73 @@ public class MapFragment extends SupportMapFragment implements OnMapReadyCallbac
 //                .findFragmentById(R.id.mapnew);
         getMapAsync(this);
     }
-*/
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        super.onCreateView(inflater, container, savedInstanceState);
-        v=inflater.inflate(R.layout.fragment_map, null);
 
-        return v;
-}
 
-//    @Nullable
 //    @Override
-//    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-//        View view = inflater.inflate(R.layout.fragment_map, container, false);
-//
-//        gMapView = (MapView) view.findViewById(R.id.mapnew);
-//        gMapView.getMapAsync(this);
-//
-//        MapsInitializer.initialize(getContext());
-//        return view;
-//    }
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        super.onCreateView(inflater, container, savedInstanceState);
+//        v=inflater.inflate(R.layout.fragment_map, null);
+//        getMapAsync(this);
+//        return v;
+//}
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setMaxZoomPreference(19f);
+        mMap.setMinZoomPreference(15f);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        insertAllMarks();
+    }
+
+    public void insertAllMarks()
+    {
+        mMap.clear();
+        heatMapMarkers.clear();
+
+        Log.d("MapFragment","Inserting all Marks!");
+        int i = 0;
+        for (EventDTO evento: DataStore.sharedInstance().currentEvents) {
+            LatLng position =new LatLng(
+                    Double.parseDouble(evento.getLocationDTO().getLy()),
+                    Double.parseDouble(evento.getLocationDTO().getLx()));
+            heatMapMarkers.add(position);
+
+            mMap.addMarker(
+                    new MarkerOptions().position(position
+                    ).title(evento.getName()).snippet(evento.getDescription())
+            );
+
+            if(i++ == DataStore.sharedInstance().currentEvents.size() - 1){
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
+            }
+            Log.d("MapFragment","Event: "+evento.toString());
+        }
+        insertHeatMap();
+    }
+
+    public void insertHeatMap()
+    {
+        int[] colors = {
+                Color.rgb(255, 211, 0), // yellow
+                Color.rgb(255, 0, 0)    // red
+        };
+
+        float[] startPoints = {
+                0.2f, 1f
+        };
+        Gradient gradient = new Gradient(colors, startPoints);
+
+        mProvider = new HeatmapTileProvider.Builder()
+                .data(heatMapMarkers)
+                .gradient(gradient)
+                .build();
+        mProvider.setRadius(30);
+
+        mOverlay = mMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
+
+        mProvider.setOpacity(0.5);
+        mOverlay.clearTileCache();
     }
 }
