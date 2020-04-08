73447ed5ac7b9c954595ef717ab0eadcf8d4f8cc
Felipe Junges <felipe.junges@pontomais.com.br>
2020-04-07 22:57:31 -0300

    Profile

---
+++ app/src/main/java/com/andrevalvassori/segnum2020/Controller/UserProfileFragments/ProfileFragment.java
@@ -0,0 +1,16 @@
+package com.andrevalvassori.segnum2020.Controller.UserProfileFragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.andrevalvassori.segnum2020.R;
+
+public class ProfileFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_profile);
+    }
+}
